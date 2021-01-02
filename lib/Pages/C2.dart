47ff69f6b687ae97711750e50b28cd45287f5734
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2021-01-02 12:41:48 -0800

    Commit

---
+++ lib/Pages/C2.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class C2 extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    Future<bool> _onBackPressed() {
+      Navigator.pushNamed(context, '/home');
+    }
+
+    return new WillPopScope(
+      onWillPop: _onBackPressed,
+      child: GestureDetector(
+        onPanUpdate: (DragUpdateDetails details) {
+          if(details.delta.dy > 0)
+            Navigator.pushNamed(context, '/2');
+        },
+        child: Scaffold(
+          backgroundColor: Color(0xFFCADCED),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Icon(
+                    Icons.keyboard_arrow_up,
+                    size: 100,
+                  ),
+                  margin: EdgeInsets.only(top: 50),
+                ),
+                Container(
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.deepPurple,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('assets/images/man.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Pages/HomePage.dart';
-import 'Pages/C.dart';
+import 'Pages/C1.dart';
+import 'Pages/C2.dart';
 import 'Pages/P.dart';
 import 'Pages/G1.dart';
 import 'Pages/G2.dart';
@@ -11,79 +12,11 @@ void main() {
     initialRoute: '/home',
     routes: {
       '/home': (context) => HomePage(),
-      '/1': (context) => C(),
-      '/2': (context) => P(),
-      '/3': (context) => G1(),
-      '/4': (context) => G2(),
+      '/1': (context) => C1(),
+      '/2': (context) => C2(),
+      '/3': (context) => P(),
+      '/4': (context) => G1(),
+      '/5': (context) => G2(),
     },
   ));
 }
-
-/*
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-*/
